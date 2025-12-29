be12ac23bc40d83d79b5af15d35607b0dee23de2
Raffy Zulfan Firdaus <140870663+Raffyzf@users.noreply.github.com>
2025-12-30 00:29:31 +0700

    Delete laragon/www/kuliah/Pertemuan_12 directory

--- laragon/www/kuliah/Pertemuan_12/user/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../koneksi.php";
-$id = $_GET['id'];
-mysqli_query($koneksi, "DELETE FROM raffy_users WHERE id_user = '$id'");
-echo"<script>
-            alert('Delete User Berhasil');
-            window.location.href = 'users.php';
-    </script>";
-?>
